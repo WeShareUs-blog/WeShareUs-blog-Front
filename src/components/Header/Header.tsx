b5b344dd4dd331ff1e9bf69b27a1a14ec05919de
jeangho293 <jeangho293@naver.com>
2023-02-03 20:33:19 +0900

    [feat]: 로그아웃 기능 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,31 @@
-import { AppBar, Stack, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { LinkButton } from '../LinkButton';
 import { today } from '../../libs/dayjs';
 
 function Header() {
   // 1. destructure props
   // 2. lib hooks
+  const navigation = useNavigate();
+
   // 3. state hooks
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
   // 4. query hooks
   // 5. form hooks
   // 6. calculate values
+  const open = Boolean(anchorEl);
+
   // 7. effect hooks
   // 8. handlers
   return (
@@ -27,7 +43,7 @@ function Header() {
           }}
         >
           <Stack direction="row" spacing={8} sx={{ alignItems: 'center' }}>
-            <Typography variant="h4">We Share US</Typography>
+            <Typography variant="h4">We Share Us</Typography>
             <Stack direction="row" spacing={4}>
               <LinkButton to="./post">게시글</LinkButton>
               <LinkButton to={`./todo?publishedDate=${today()}`}>
@@ -35,7 +51,30 @@ function Header() {
               </LinkButton>
             </Stack>
           </Stack>
-          <AccountCircleIcon />
+          <IconButton
+            id="profile-button"
+            onClick={(event) => setAnchorEl(event.currentTarget)}
+            aria-haspopup="true"
+            sx={{ color: '#FFF' }}
+          >
+            <AccountCircleIcon sx={{ width: '32px', height: '32px' }} />
+          </IconButton>
+          <Menu
+            aria-labelledby="profile-button"
+            open={open}
+            anchorEl={anchorEl}
+            onClose={() => setAnchorEl(null)}
+          >
+            <MenuItem
+              onClick={() => {
+                localStorage.clear();
+                navigation('/login');
+              }}
+              sx={{ fontSize: '14px', padding: '0 12px' }}
+            >
+              로그아웃
+            </MenuItem>
+          </Menu>
         </Stack>
       </Toolbar>
     </AppBar>
