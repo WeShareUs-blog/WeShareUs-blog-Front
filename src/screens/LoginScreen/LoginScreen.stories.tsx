1e125311037e8e4f12e0c41bee532779c358917a
jeangho293 <jeangho293@naver.com>
2023-02-05 21:14:27 +0900

    WBF-11 [fix]: interation 오류 때문에 우선 제거하고 사용

--- src/screens/LoginScreen/LoginScreen.stories.tsx
+++ src/screens/LoginScreen/LoginScreen.stories.tsx
@@ -1,8 +1,6 @@
 import { ComponentProps } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
-import { expect } from '@storybook/jest';
 import { rest } from 'msw';
-import { within, userEvent, waitFor } from '@storybook/testing-library';
 import { LoginScreen } from './LoginScreen';
 
 type ArgsTypes = ComponentProps<typeof LoginScreen>;
@@ -22,39 +20,3 @@ export default {
 } as Meta<ArgsTypes>;
 
 export const Default: StoryObj<ArgsTypes> = {};
-
-export const FailLogin: StoryObj<ArgsTypes> = {
-  parameters: {
-    msw: {
-      handlers: {
-        login: rest.post('http://localhost:4000/users/login', (req, res, ctx) =>
-          res(
-            ctx.status(400),
-            ctx.json({ errorMessage: '아이디 또는 비밀번호가 틀렸습니다.' }),
-          ),
-        ),
-      },
-    },
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-
-    const accountTextInput = await canvas.findByPlaceholderText('아이디');
-    await userEvent.type(accountTextInput, 'testID');
-
-    const passwordTextInput = await canvas.findByPlaceholderText('비밀번호');
-    await userEvent.type(passwordTextInput, '1234');
-
-    const loginButton = await canvas.findByRole('button', {
-      name: '로그인하기',
-    });
-
-    await waitFor(async () => {
-      await userEvent.click(loginButton);
-      // const errorMessageText = await canvas.findByText(
-      //   '아이디 또는 비밀번호가 틀렸습니다.',
-      // );
-      // expect(errorMessageText).toBeInTheDocument();
-    });
-  },
-};
