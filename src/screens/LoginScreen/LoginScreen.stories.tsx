39a7a52e30291625406bd1540b8f55d1d67e3c79
jeangho293 <jeangho293@naver.com>
2023-02-05 19:11:59 +0900

    WBF-11 [test]: jest 추가하여 인터렉션 테스트

--- src/screens/LoginScreen/LoginScreen.stories.tsx
+++ src/screens/LoginScreen/LoginScreen.stories.tsx
@@ -1,6 +1,8 @@
 import { ComponentProps } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
+import { expect } from '@storybook/jest';
 import { rest } from 'msw';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
 import { LoginScreen } from './LoginScreen';
 
 type ArgsTypes = ComponentProps<typeof LoginScreen>;
@@ -33,5 +35,25 @@ export const FailLogin: StoryObj<ArgsTypes> = {
         ),
       },
     },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const accountTextInput = await canvas.findByPlaceholderText('아이디');
+    await userEvent.type(accountTextInput, 'testID');
+
+    const passwordTextInput = await canvas.findByPlaceholderText('비밀번호');
+    await userEvent.type(passwordTextInput, '1234');
+
+    const loginButton = await canvas.findByRole('button', {
+      name: '로그인하기',
+    });
+
+    // NOTE: 버튼을 누를 때까지 계속 누른다...
+    await waitFor(() => userEvent.click(loginButton));
+    const errorMessageText = await canvas.findByText(
+      '아이디 또는 비밀번호가 틀렸습니다.',
+    );
+    await expect(errorMessageText).toBeInTheDocument();
   },
 };
