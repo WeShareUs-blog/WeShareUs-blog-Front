bb88958538f066866aa129939c5db22212839e29
jeangho293 <jeangho293@naver.com>
2023-02-19 15:29:06 +0900

    [fix]: initialize set comment whene add todo list

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -61,6 +61,7 @@ function TodoList(props: { todo: Todo }) {
       await updateTodo({
         variables: { id, publishedDate, todoItems },
       });
+      setContent('');
     })();
   };
   const handleAppend = () => {
